bc72bd8c3791b02ba91ec4428fdec5c7b8914918
Yifan Luo <jeffluoyifan@gmail.com>
2024-08-16 19:42:57 +1000

    Update cleaned data

--- utils/combining.py
+++ utils/combining.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-df_commercial = pd.read_csv('content_Commercial.csv')
-df_abc = pd.read_csv('content_ABC.csv')
+df_commercial = pd.read_csv('/Users/yifan/Documents/GitHub/datasets/ART/cleaned_dataset/content_Commercial.csv')
+df_abc = pd.read_csv('/Users/yifan/Documents/GitHub/datasets/ART/cleaned_dataset/content_ABC.csv')
 
 
 df_combined = pd.concat([df_commercial, df_abc], ignore_index=True)
